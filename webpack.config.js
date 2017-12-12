f87e122362a03bab407bf6b9637c9e854edbd299
Danny Straus <strausd@me.com>
2017-12-12 07:58:33 -0600

    Setup webpack for prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,36 @@
 const path = require('path');
 const public_path = path.join(__dirname, 'public');
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: public_path,
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                loader: 'babel-loader',
-                test: /\.js$/,
-                exclude: /node_modules/
-            },
-            {
-                test: /\.s?css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
-        ]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: public_path,
-        historyApiFallback: true
-    }
+module.exports = (env, argv) => {
+    const isProduction = env == 'production';
+
+    return {
+        entry: './src/app.js',
+        output: {
+            path: public_path,
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    loader: 'babel-loader',
+                    test: /\.js$/,
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.s?css$/,
+                    use: [
+                        'style-loader',
+                        'css-loader',
+                        'sass-loader'
+                    ]
+                }
+            ]
+        },
+        devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+        devServer: {
+            contentBase: public_path,
+            historyApiFallback: true
+        }
+    };
 };
